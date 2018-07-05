e3720574fa73c930eaaf6c35a6871162bebecfe2
Ian Garrison <img4@cornell.edu>
2018-07-05 14:10:57 -0500

    REJECT combined SAT score below 1000

--- College-Application/Java/src/main/java/io/sketchdev/katas/CollegeApplicationReviewer.java
+++ College-Application/Java/src/main/java/io/sketchdev/katas/CollegeApplicationReviewer.java
@@ -1,5 +1,18 @@
 package io.sketchdev.katas;
 
 public class CollegeApplicationReviewer {
-    // TODO: Implement auto review
+
+    public ReviewDecision evaluateSATScore(int mathScore, int readingScore) {
+
+        int combinedScore = mathScore + readingScore;
+
+
+        if (combinedScore < 1000) {
+            return ReviewDecision.REJECT;
+
+        }
+        else
+            return ReviewDecision.UNDECIDED;
+
+    }
 }
--- College-Application/Java/src/test/java/io/sketchdev/katas/CollegeApplicationReviewerTest.java
+++ College-Application/Java/src/test/java/io/sketchdev/katas/CollegeApplicationReviewerTest.java
@@ -5,5 +5,32 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CollegeApplicationReviewerTest {
-    // TODO: Test auto review
+/*
+    @Test
+    public void TestEmailMissingTld_IsNotSaved()
+    {
+        boolean saved = new CollegeApplicationForm().Apply("J", "W", "wheelie33@gmail");
+        assertFalse(saved);
+    }
+*/
+    @Test
+    public void testUndecidedSATScore() {
+        ReviewDecision decision = new CollegeApplicationReviewer().evaluateSATScore(600, 600);
+        assertEquals(ReviewDecision.UNDECIDED, decision);
+    }
+
+    @Test
+    public void testRejectSATScore() {
+        ReviewDecision decision = new CollegeApplicationReviewer().evaluateSATScore(500, 499);
+        assertEquals(ReviewDecision.REJECT, decision);
+    }
+
+    @Test
+    public void testFailSATScore() {
+        ReviewDecision decision = new CollegeApplicationReviewer().evaluateSATScore(500, 500);
+        assertEquals(ReviewDecision.UNDECIDED, decision);
+    }
+
+
+
 }
