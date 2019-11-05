30a4c09f68c8417f65d89d65865e7eed49893afd
Ian Garrison <img4@cornell.edu>
2019-11-05 14:46:35 -0600

    done

--- College-Application/Java/src/main/java/io/sketchdev/katas/CollegeApplicationReviewer.java
+++ College-Application/Java/src/main/java/io/sketchdev/katas/CollegeApplicationReviewer.java
@@ -1,5 +1,19 @@
 package io.sketchdev.katas;
 
 public class CollegeApplicationReviewer {
-    // TODO: Implement auto review
+
+
+    public ReviewDecision autoReview(int reading, int math) {
+        if(math<400){
+            return ReviewDecision.REJECT;
+        }
+        if(reading+math<1000){
+            return ReviewDecision.REJECT;}
+            if (reading >= 600 && math >= 750) {
+            return ReviewDecision.ACCEPT;
+            }
+
+        return ReviewDecision.UNDECIDED;
+
+    }
 }
--- College-Application/Java/src/test/java/io/sketchdev/katas/CollegeApplicationReviewerTest.java
+++ College-Application/Java/src/test/java/io/sketchdev/katas/CollegeApplicationReviewerTest.java
@@ -3,7 +3,38 @@ package io.sketchdev.katas;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-
 public class CollegeApplicationReviewerTest {
-    // TODO: Test auto review
+    private int reading, math;
+    @Test
+    public void autoRejectTest() {
+        reading=499;
+        math=500;
+        CollegeApplicationReviewer tester=new CollegeApplicationReviewer();
+        assertEquals(ReviewDecision.REJECT,tester.autoReview(reading, math));
+
+    }
+    @Test
+    public void autoUndecidedTest() {
+        reading=600;
+        math=400;
+        CollegeApplicationReviewer tester=new CollegeApplicationReviewer();
+        assertEquals(ReviewDecision.UNDECIDED,tester.autoReview(reading, math));
+
+    }
+    @Test
+    public void autoMathRejectTest(){
+        math=399;
+        reading=601;
+        CollegeApplicationReviewer tester=new CollegeApplicationReviewer();
+        assertEquals(ReviewDecision.REJECT,tester.autoReview(reading, math));
+    }
+    @Test
+    public void autoApproveTest() {
+        reading =600;
+        math=750;
+        CollegeApplicationReviewer tester=new CollegeApplicationReviewer();
+        assertEquals(ReviewDecision.ACCEPT,tester.autoReview(reading, math));
+
+    }
+
 }
